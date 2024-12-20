c8a7da56e1b8945b47f6736f1dee74c821904d19
Hotl7 <Natasha.Mandrykina@outlook.com>
2024-12-20 11:39:05 +0000

    f

---
+++ lab1/lab1.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <pthread.h>
+#include <unistd.h>
+
+using namespace std;
+
+pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
+pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
+int ready = 0;
+
+void* producer(void*){
+	pthread_mutex_lock(&lock);
+	if (ready == 1) {
+		pthread_mutex_unlock(&lock);
+	}
+	ready = 1;
+	cout<<"Поставщик отправляет событие"<<endl;
+	pthread_cond_signal(&cond);
+	pthread_mutex_unlock(&lock);
+	
+	return nullptr;
+	}
+
+void* consumer(void*){
+
+	pthread_mutex_lock(&lock);
+	while(ready == 0) {
+		pthread_cond_wait(&cond,&lock);
+	}
+	ready = 0;
+	cout<<" Потребитель получил событие"<<endl;
+	pthread_mutex_unlock(&lock);
+
+	return nullptr;
+}
+
+int main() {
+	pthread_t producerThread,consumerThread;
+	
+	pthread_create(&producerThread,nullptr,producer,nullptr);
+	pthread_create(&consumerThread,nullptr,consumer,nullptr);
+	
+	pthread_mutex_destroy(&lock);
+	pthread_cond_destroy(&cond);
+
+	pthread_join(consumerThread,NULL);
+
+	return 0;
+}
+
